f20e0f6d71ba70fb2534ea7ba50fea8ad9b59a37
Servio Palacios <servio@palacios.com>
2018-08-28 13:55:44 -0400

    Updates

--- src/controller.js
+++
@@ -1,7 +0,0 @@
-import { Controller } from 'cerebral';
-import app from './modules/app';
-import Devtools from 'cerebral/devtools';
-
-export default Controller(app, {
-	devtools: process.env.NODE_ENV === 'production' ? null : Devtools({ host: 'localhost:8787' }),
-});
